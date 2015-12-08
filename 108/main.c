76e90aaa517140018c5294e5b00264e1679a4011
4418481470 <4418481470@ci026101m.ecc.u-tokyo.ac.jp>
2015-12-08 20:51:17 +0900

    108

--- 108/main.c
+++ 108/main.c
@@ -3,6 +3,11 @@ extern int maxof3(int x, int y, int z);
 
 int main(int argc, char const* argv[])
 {
-	printf("%d\n",maxof3(1,5,3));
+	printf("maxof3(%d,%d,%d) = %d\n", 1, 2, 3, maxof3(1,2,3));
+	printf("maxof3(%d,%d,%d) = %d\n", 1, 3, 2, maxof3(1,3,2));
+	printf("maxof3(%d,%d,%d) = %d\n", 2, 1, 3, maxof3(2,1,3));
+	printf("maxof3(%d,%d,%d) = %d\n", 2, 3, 1, maxof3(2,3,1));
+	printf("maxof3(%d,%d,%d) = %d\n", 3, 1, 2, maxof3(3,1,2));
+	printf("maxof3(%d,%d,%d) = %d\n", 3, 2, 1, maxof3(3,2,1));
 	return 0;
 }
--- 108/maxof3.s
+++ 108/maxof3.s
@@ -2,42 +2,25 @@
 	.align 2
 	.globl maxof3, maxof2
 maxof3:
-	mflr 0
-	std 0,16(1)
-	std 31,-8(1)
-	stdu 1,-144(1)
-	mr 31,1
-	mr 8,3
-	mr 10,4
-	mr 9,5
-	stw 8,192(31)
-	stw 10,200(31)
-	stw 9,208(31)
-	lwz 9,192(31)
-	extsw 10,9
-	lwz 9,200(31)
-	extsw 9,9
-	mr 3,10
-	mr 4,9
-	bl maxof2
-	nop
-	mr 9,3
-	stw 9,112(31)
-	lwz 9,112(31)
-	extsw 10,9
-	lwz 9,208(31)
-	extsw 9,9
-	mr 3,10
-	mr 4,9
-	bl maxof2
-	nop
-	mr 9,3
-	mr 3,9
-	addi 1,31,144
-	ld 0,16(1)
-	mtlr 0
-	ld 31,-8(1)
+	mflr %r0               # %r0 <- LR
+	std  %r0,  16(%r1)     # %r0 -> M(%r1+16)   LRを退避
+	stdu %r1,  -144(%r1)   # スタックをのばす
+	std  %r31, -8(%r1)     # %r31 -> M(%r1-8)   %r31は不揮発なので退避
+	mr   %r31, %r1         # %r31 <- %r1        スタックの底をさす
+	
+	stw  %r3,  112(%r31)   # 底から112にxを退避
+	stw  %r4,  120(%r31)   # 底から120にyを退避
+	stw  %r5,  128(%r31)   # 底から128にzを退避
+	bl maxof2              # %r3  <- maxof2(x,y)
+	lwz  %r4,  128(%r31)   # %r4  <- z          %r31は変化がないことが保証されている(不揮発)
+	bl maxof2              # %r3  <- maxof3(x,y,z)
+	
+	addi %r1,  %r31, 144   # スタックを戻す
+	ld   %r0,  16(%r1)     # %r0 <- M(%r1+16)   退避していたLRをload
+	mtlr %r0               # %r0 -> LR
+	ld   %r31, -8(%r1)     # %r31 <- M(%r1-8)   退避していた古い%r31を戻す
 	blr
+
 maxof2:
 	mr   %r10, %r3
 	mr   %r9,  %r4
